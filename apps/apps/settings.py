e613a3daf3f9253ff204ca12312b6625ae87db23
luckinlee <1348814167@qq.com>
2020-10-06 17:42:51 +0800

    修改数据库链接

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -72,23 +72,23 @@ WSGI_APPLICATION = 'apps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'apps',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'PORT': '3306',
-        'HOST': '127.0.0.1',
-        'CHARSET': 'utf8'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'apps',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'PORT': '3306',
+#         'HOST': '127.0.0.1',
+#         'CHARSET': 'utf8'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
