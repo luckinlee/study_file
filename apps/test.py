332551bd280d3eb359a21b6bc4ac022d982b534c
TAL <1348814167@qq.com>
2020-10-13 12:41:35 +0800

    test

--- apps/test.py
+++ apps/test.py
@@ -1,7 +1,9 @@
 import os
-import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.settings")
     import django
     django.setup()
+    from app01 import models
+    rel = models.Author.objects.all()
+    print(rel.query)
