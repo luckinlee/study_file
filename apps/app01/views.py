fb2f63d6d093772706c3e2f34e7287bef4462b90
TAL <1348814167@qq.com>
2020-11-02 10:24:45 +0800

    登录v1

--- apps/app01/views.py
+++ apps/app01/views.py
@@ -4,6 +4,7 @@ from app01.myforms.register_forms import RegForm
 from app01 import models
 from django.http import JsonResponse
 from app01.common.code import ret_code_img
+from django.contrib import auth
 
 
 class Register(View):
@@ -23,6 +24,7 @@ class Register(View):
             if file_obj:
                 clean_data['avatar'] = file_obj
             models.UserInfo.objects.create_user(**clean_data)
+            back_dic['url'] = '/login/'
         else:
             back_dic['code'] = 10001
             back_dic['msg'] = user_obj.errors
@@ -32,6 +34,24 @@ class Register(View):
 class Login(View):
     def get(self, request):
         return render(request, 'login.html', locals())
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        code = request.POST.get('code')
+        back_dict = {}
+        if request.session.get('code').lower() == code.lower():
+            user_obj = auth.authenticate(request, username=username, password=password)
+            if user_obj:
+                auth.login(request, user_obj)
+                back_dict['url'] = '/home/',
+            else:
+                back_dict['error_code'] = 10001
+                back_dict['msg'] = '用户名或密码不正确'
+        else:
+            back_dict['error_code'] = 10002
+            back_dict['msg'] = '验证码不正确'
+        return JsonResponse(back_dict, safe=False)
 
 
 def get_code(request):
---
+++ apps/static/js/get_csrf.js
@@ -0,0 +1,32 @@
+//自己写一个getCookie方法：
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
+//可以使用$.ajaxSetup()方法为ajax请求统一设置
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
